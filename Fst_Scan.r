c7dbfad49d13c09da151f5d19bcaea29c353c30c
Elise J Gay <41110622+EliseGAY@users.noreply.github.com>
2026-03-13 14:59:37 +0100

    Update Fst_Scan.r

--- Fst_Scan.r
+++ Fst_Scan.r
@@ -18,8 +18,8 @@ library(gridExtra)
 library(stringr)
 library(devtools)
 library(GenomicRanges)
-system("git clone https://github.com/EliseGAY/Package_VCF2PopStructure.git")
-load_all("Package_VCF2PopStructure/")
+# system("git clone https://github.com/EliseGAY/Package_VCF2PopStructure.git")
+load_all("../Package_VCF2PopStructure/")
 
 #===============================#
 #===============================#
@@ -47,24 +47,24 @@ metadata=as.data.frame(metadata)
 pop=unique(metadata$Population)
 
 # read chr length
-table_chr=read.table("Scaff_length_sorted.list", header = T)
+table_chr=read.table("metadata/table_chr_length.txt", header = T)
 
 #-----------------------------------------------------------#
 # Generate Genotype tables needed in different R packages
 #-----------------------------------------------------------#
 
 # Read the VCF with vcfR :
-VCFR_data=read.vcfR("/data/VCF_example.NoNa.SNP.vcf.gz")
+VCFR_data=read.vcfR("data/Chr1_Example.vcf.gz")
 
 # create a pop sorted by VCF colnames
-metadata_sorted <- metadata[match(colnames(VCFR_data@gt)[-1], metadata$GT_sample),]
-pop_list = split(metadata_sorted$GT_sample, metadata_sorted$Population)
+metadata_sorted <- metadata[match(colnames(VCFR_data@gt)[-1], metadata$sample),]
+pop_list = split(metadata_sorted$sample, metadata_sorted$Social_Morph)
 
 # current_chr
 chr = arg[1]
 # to test in local example 
-chr = "ptg000007l"
-chr_len = table_chr[which(table_chr$scaffold == chr),]$length
+chr = "CHR1"
+chr_len = table_chr[which(table_chr$Chr == chr),]$length
 
 #===============================================#
 #===============================================#
@@ -84,24 +84,43 @@ loci_table = as.data.frame(loci_table)
 geno_table = Convert_GT(GT_table = loci_table)
 
 # geno_table[c(1:10),c(1:10)]
-#'                ptg000007l_8628 ptg000007l_8631 ptg000007l_8633 ptg000007l_8639 ptg000007l_9618 ptg000007l_9622 ptg000007l_9625 ptg000007l_9626
-#'1622W1_S254               0               0               0               0              NA              NA              NA              NA
-#'1623W1_S276               0               0               0               0              NA              NA              NA              NA
-#'1624W1_S392               0               0               0               0              NA               0               0               0
-#'1625Q_S431                0               0               0               0              NA              NA              NA              NA
-#'1625W1_S384               0               0               0               0               1               1               1               1
+#                10_Q_S_A_S9 12_Q_S_A_S11 13_Q_A_S12 14_Q_A_S13 15_Q_A_S14 16_Q_A_S15 17_Q_A_S16 18_Q_A_S17 19_Q_A_S18 1_Q_S_A_S1
+#CHR1_159039           0            1          0          1          0          0          0          0          0          0
+#CHR1_219063           0            0          0          0          0          0          0          0          0          0
+#CHR1_254450           0            0          1          0          0          0          0          0          0          0
+#CHR1_359471           0            1          0          1          0          1          0          1          1          2
+#CHR1_376366           0            0          0          0          0          0          0          0          0          0
+#CHR1_416191           1            2          1          1          1          2          2          2          1          2
+#CHR1_464593           0            0          0          0          0          0          0          0          0          0
+#CHR1_486240           0            0          1          0          0          0          0          0          0          0
+#CHR1_493425           0            0          0          0          0          0          1          0          0          0
+#CHR1_514070           0            0          0          0          1          0          0          0          0          0
 
 # get SW table :
-sw_data = Get_SW_abs(VCFR_data, slide = 5000, window = 10000)
-min_snp = 50
+sw_data = Get_SW_abs(VCFR_data, slide = 200000, window = 500000)
 
 # Prepare table to dynamically adapt to the number of pairwise comparisons
-pop_pairs_table= getPairwisePop(pop_vec = sort(unique(metadata_sorted$Population)))
+pop_pairs_table = getPairwisePop(pop_vec = sort(unique(metadata_sorted$Social_Morph)))
+pop_pairs_table
+#   V1       V2
+# monogyne polygyne
+
+# initialize your variable
 pop_pairs_names = paste(pop_pairs_table$V1,pop_pairs_table$V2, sep = "_" )
+# [1] "monogyne_polygyne"
+
 sw_data[, pop_pairs_names] <- NA_real_
 colcount = which((colnames(sw_data) %in% pop_pairs_names))
+head(sw_data)
+# chr   start     end mid_point nb_snp monogyne_polygyne
+#1 CHR1       1  500000  250000.5      9                NA
+#2 CHR1  200001  700000  450000.5     22                NA
+#3 CHR1  400001  900000  650000.5     34                NA
+#4 CHR1  600001 1100000  850000.5     45                NA
+#5 CHR1  800001 1300000 1050000.5     52                NA
+#6 CHR1 1000001 1500000 1250000.5     56                NA
 
-# compute Fst
+# compute Fst for each windows
 for(i in 1:nrow(sw_data)){
   if(sw_data[i,]$nb_snp >= min_snp){
     pos_start = sw_data[i, "start"]
@@ -114,33 +133,35 @@ for(i in 1:nrow(sw_data)){
     sw_data[i, colcount] = Fst_all
   }
 }
+# fix colnames
 colnames(sw_data) = c(colnames(sw_data)[1:5], names(Fst_all))
+
 # save
-write.table(sw_data, "Sliding_Fst/Hudson_Fst_scans.txt")
+write.table(sw_data, paste("Sliding_Fst/", chr, "_Hudson_Fst_scans.txt", sep = ""))
 
 #----------#
 # Plot FST
 #----------#
 # pair to plot
-pairs_FST="Cesseras_Cucugnan"
-
+pairs_FST=colnames(sw_data)[6]
 
 # read the FST indices
-sw_data=read.table("SlidingFst/Hudson_Fst_scans.txt", header = TRUE)
-# read chr length
-length_chr=table_chr[which(table_chr$Chr==chr),"length"]
-  
+sw_data=read.table(paste("Sliding_Fst/", chr, "_Hudson_Fst_scans.txt"), header = TRUE)
+
 # Format table : discard negative value
 sw_data[sw_data < 0] = 0
 
 # plot
 plot_name=paste(chr, "_FST_",pairs_FST, sep="")
+head(sw_data)
 
 p = ggplot(data = sw_data) + 
   geom_point(aes_string(x="mid_point",y=pairs_FST),
-             alpha = 0.6,
-             color = "darkred",
-             pch=1) +
+             alpha = 0.5,
+             fill = "lightblue3",
+             color = "lightcyan4",
+             size = 2,
+             pch=21) +
 
   labs(y="FST", x = chr)+
   ggtitle(plot_name) +
@@ -161,8 +182,8 @@ p = ggplot(data = sw_data) +
                                   by = round(length_chr/15))) +
   scale_y_continuous(breaks = seq(0, 1, 
                                   by = 0.1), limits = c(0,1))
-
-ggsave(p, path="Sliding_Fst/", width = 12, height = 10, device = "pdf", filename = paste(chr, "Fst_scans.pdf",sep = "_"))
+p
+ggsave(p, path="Sliding_Fst/", width = 10, height = 5, device = "pdf", filename = paste(chr, "Fst_scans.pdf",sep = "_"))
 
 #========================#
 #========================#
@@ -182,7 +203,7 @@ Fst_by_SNP = getFstBySNP_Count(loci_table_T = geno_table, pop_table = metadata_s
 
 # 2. Create a global table with all position and the Fst pairs results :
 
-# get all intersect of all position
+# get all intersect of all position in the pairwise comparisons
 position=sort(unique(unlist(Fst_by_SNP[which(grepl(pattern = "position", names(Fst_by_SNP)))])))
 
 # initiate dataframe
@@ -201,31 +222,19 @@ for(name in names(Fst_by_SNP)){
 }
 
 # check for restuls :
-#position Termes_Cesseras Termes_Cucugnan Cesseras_Cucugnan
-#1       9678     0.503759398              NA       0.590062112
-#2       9728              NA    -0.043478261       0.092627599
-#3      10382     0.503759398              NA       0.590062112
-#4      10434              NA    -0.043478261       0.092627599
-#5      10484     0.029573935     0.306666667                NA
-#6      10982     0.719298246     0.733333333                NA
+#position polygyne_monogyne
+#1   159039       0.071784891
+#2   359471      -0.014948461
+#3   416191      -0.041384288
+#4   493425      -0.073682407
 
-# head(Fst_by_SNP$Termes_Cesseras)
-# head(Fst_by_SNP$Termes_Cesseras_position)
+head(Fst_by_SNP$polygyne_monogyne_position)
+head(Fst_by_SNP$polygyne_monogyne)
 # [1] 0.50375940 0.50375940 0.02957393 0.71929825 0.17293233 0.71929825
 # [1]  9678 10382 10484 10982 11771 13963
 
-# head(Fst_by_SNP$Termes_Cucugnan)
-# [1] -0.04347826 -0.04347826  0.30666667  0.73333333 -0.09565217 -0.04347826
-# head(Fst_by_SNP$Termes_Cucugnan_position)
-# [1]  9728 10434 10484 10982 11761 11931
-
-# head(Fst_by_SNP$Cesseras_Cucugnan)
-# [1] 0.59006211 0.09262760 0.59006211 0.09262760 0.04725898 0.31677019
-# head(Fst_by_SNP$Cesseras_Cucugnan_position)
-# [1]  9678  9728 10382 10434 11761 11771
-
 # save the table 
-write.table(all_fst, "Sliding_Fst/Hudson_Fst_bySNPs.txt")
+write.table(all_fst, paste("Sliding_Fst/", chr, "_Hudson_Fst_bySNPs.txt", sep =""))
 
 #-----------------------------#
 # Plot FST
@@ -240,15 +249,21 @@ summary(all_fst)
 # plot
 all_fst_melt = melt(all_fst, id.vars = c("position"))
 plot_name=paste(chr, "_FST_",pairs_FST, sep="")
-
-ggplot() + 
+head(all_fst_melt)
+p=ggplot() + 
   geom_point(aes(x=all_fst_melt$position,y=all_fst_melt$value, 
                  group = all_fst_melt$variable, 
-                 colour = all_fst_melt$variable),
-             alpha = 0.9,
-             pch=1) +
+                 colour = all_fst_melt$variable,
+                 fill = all_fst_melt$variable),
+             alpha = 0.4,
+             size = 1,
+             pch=21) +
   
-  labs(y="FST", x = chr)+
+  labs(y = "FST", x = chr, colour = "Pop pairs", fill = "Pop pairs") +
+  
+  scale_colour_brewer(palette = "Dark2") +
+  scale_fill_brewer(palette = "Dark2") +
+  
   ggtitle(plot_name) +
   theme(axis.text.x = element_text(angle = 90, 
                                    vjust = 0.5, 
@@ -267,4 +282,5 @@ ggplot() +
   scale_y_continuous(breaks = seq(0, 1, 
                                   by = 0.1), limits = c(0,1))
 
-ggsave(p, path="Sliding_Fst/", width = 12, height = 10, device = "pdf", filename = paste(chr, "Fst_BySNP_scans.pdf",sep = "_"))
+
+ggsave(p, path="Sliding_Fst/", width = 10, height = 5, device = "pdf", filename = paste(chr, "Fst_BySNP_scans.pdf",sep = "_"))
